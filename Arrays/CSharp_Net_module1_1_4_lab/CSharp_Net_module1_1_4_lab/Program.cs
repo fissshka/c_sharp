ea662e7965fce885f126186760cb37cc7109d053
Nataliya Prisych <nataliya.prisych@gmail.com>
2018-01-30 18:23:27 +0200

    First commit

---
+++ Arrays/CSharp_Net_module1_1_4_lab/CSharp_Net_module1_1_4_lab/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_1_4_lab
+{
+    class Program
+    {
+        // 1) declare enum ComputerType
+        enum ComputerType { Desktop, Laptop, Server};
+
+        // 2) declare struct Computer
+        struct Computer
+        {
+            public int CPU;
+            double HGz;
+            public int memory;
+            public int HDD;
+        }
+
+        static void Main(string[] args)
+        {
+            // 3) declare jagged array of computers size 4 (4 departments)
+            int[][] comps_per_dep = new int[4][];
+            // 4) set the size of every array in jagged array (number of computers)
+            comps_per_dep[0] = new int[3];
+            comps_per_dep[1] = new int[3];
+            comps_per_dep[2] = new int[3];
+            comps_per_dep[3] = new int[3];
+            // 5) initialize array
+            comps_per_dep[0] = new int[] { 2, 2, 1 };
+            comps_per_dep[1] = new int[] { 0, 3, 0 };
+            comps_per_dep[2] = new int[] { 3, 2, 0 };
+            comps_per_dep[3] = new int[] { 1, 1, 2 };
+            // Keep the console window open in debug mode.
+            // Note: use loops and if-else statements
+            // 6) count total number of every type of computers
+            for (int i = 0; i < comps_per_dep.[0]; i++)
+            {
+                System.Console.Write("Dep_computers({0}): ", i);
+                for (int j = 0; j < comps_per_dep[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", comps_per_dep[i][j], j == (comps_per_dep[i].Length - 1) ? "" : " ");
+                }
+                System.Console.WriteLine();
+            }
+            // 7) count total number of all computers
+            // Note: use loops and if-else statements
+            // Note: use the same loop for 6) and 7)
+
+
+
+            // 8) find computer with the largest storage (HDD) - 
+            // compare HHD of every computer between each other; 
+            // find position of this computer in array (indexes)
+            // Note: use loops and if-else statements
+
+
+            // 9) find computer with the lowest productivity (CPU and memory) - 
+            // compare CPU and memory of every computer between each other; 
+            // find position of this computer in array (indexes)
+            // Note: use loops and if-else statements
+            // Note: use logical oerators in statement conditions
+
+
+            // 10) make desktop upgrade: change memory up to 8
+            // change value of memory to 8 for every desktop. Don't do it for other computers
+            // Note: use loops and if-else statements
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+ 
+    }
+
---
+++ CSharp_Net_module1_1_2_lab/CSharp_Net_module1_1_2_lab/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CSharp_Net_module1_1_2_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use "Debugging" and "Watch" to study variables and constants
+
+            //1) declare variables of all simple types:
+
+            // #Console.WriteLine(boo, ch, b, sb, sh, ush, i, ui, l, ul, de, fl, d0);
+
+            //bool, char, byte, sbyte, short, ushort, int, uint, long, ulong, decimal, float, double
+            // their names should be: 
+            //boo, ch, b, sb, sh, ush, i, ui, l, ul, de, fl, d0
+            // initialize them with 1, 100, 250.7, 150, 10000, -25, -223, 300, 100000.6, 8, -33.1
+            // Check results (types and values). Is possible to do initialization?
+            // Fix compilation errors (change values for impossible initialization)
+            bool boo = new bool();
+            boo = true;
+            char ch = new char();
+            ch = 'a';
+            byte b = new byte();
+            b = 1;
+            sbyte sb = new sbyte();
+            sb = 100;
+            short sh = new short();
+            sh = 2507;
+            ushort ush = new ushort();
+            ush = 150;
+            int i = new int();
+            i = 10000;
+            uint ui = new uint();
+            ui = 25u;
+            long l = new long();
+            l = 223L;
+            ulong ul = new ulong();
+            ul = 300ul;
+            decimal de = new decimal();
+            de = 100000.6m;
+            float fl = new float();
+            fl = 8f;
+            double d0 = new double();
+            d0 = -33.1d;
+            //--------------------
+            //2) declare constants of int and double. Try to change their values.
+            const int iC = 30;
+            //int iC = 20;
+            const double d1 = 0.23d;
+            //double d1 = 56.2d;
+            //--------------------
+            //3) declare 2 variables with var. Initialize them 20 and 20.5. Check types. 
+            // Try to reinitialize by 20.5 and 20 (change values). What results are there?
+            var myNew = 20.GetType();
+            var myNew1 = 20.5.GetType();
+
+            //--------------------
+            // 4) declare variables of System.Int32 and System.Double.
+            // Initialize them by values of i and d0. Is it possible?
+            //imopossible
+            //myNew = i;
+            //myNew = d0;
+
+            if (true)
+            {
+                // 5) declare variables of int, char, double 
+                // with names i, ch, do
+                // is it possible?
+                int myI = new int();
+                char myCh = new char();
+                double myD0 = new double();
+
+                // 6) change values of variables from 1)
+                i = 40;
+                int i1 = @i + 1;
+                ch = 'n';
+                //char b;
+                d0 = -0.33d;
+
+
+            }
+
+            // 7)check values of variables from 1). Are they changed? Think, why
+
+            Console.WriteLine(i);
+            Console.WriteLine(ch);
+            Console.WriteLine(d0);
+
+            // 8) use implicit/ explicit conversion to convert variables from 1). 
+            // Is it possible? 
+            //var myNum = 2;
+            //int day = 3;
+            bool boo1 = boo;
+            char ch1 = ch;
+            int newB = b;
+            short newSb = sb;
+            int newI = sh;
+            uint newUi = ush;
+            long newL1 = i;
+            long newL = ui;
+            float newF = l;
+            double newd0 = ul;
+            decimal newF1 = de;
+            double newD = fl;
+            double newDo1 = d0;
+
+            /*
+             * 
+             * 
+             * 
+             * /
+
+            
+
+            // Fix compilation errors (in case of impossible conversion commemt that line).
+            // int -> char
+            int NewIn = ch;
+            // bool -> short
+            ///short NewBoo = boo;
+            // double -> long
+            double NewLo = l;
+            // float -> char 
+            float newFLo = ch;
+            // int to char
+            int newINT = ch;
+            // decimal -> double
+                decimal newDEC = 123.56m;
+                d0 = (double)newDEC;
+            // byte -> uint
+            byte newByt = 23;
+            ui = (byte)newByt;
+            // ulong -> sbyte
+            //ulong NewULO = 456ul;
+            //sb = (ulong)NewULO;
+            // 9) and reverse conversion with fixing compilation errors.
+            ///short newSH;
+           //boo = (short)newsh;
+            // 10) declare int nullable value. Initialize it with 'null'. 
+            // Try to initialize variable i with 'null'. Is it possible?
+            int? iN;
+            iN = null;
+            //int? i = null;
+        }
+    }
+}
