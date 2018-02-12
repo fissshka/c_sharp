35d19bf6ff34f922f76eabf22385e3c79ae98cd8
Nataliya Prisych <nataliya.prisych@gmail.com>
2018-02-12 19:23:24 +0200

    Added possible solution

--- Methods and parameters/Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Box.cs
+++ Methods and parameters/Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Box.cs
@@ -21,9 +21,10 @@ namespace Hello_Cons_Dr_Methods
         //to define start position, width and height, symbol, message  according to properties
         //Use Math.Min() and Math.Max() methods
         //Use draw() to draw the box with message
+        public Box () 
         public Draw()
         {
-           
+            int x, y;
             int StartPosition = int.Parse(Console.ReadLine());
         
         }
@@ -36,3 +37,79 @@ namespace Hello_Cons_Dr_Methods
 
     }
 }
+
+
+/*
+ * public class ConsoleRectangle
+{
+    private int hWidth;
+    private int hHieght;
+    private Point hLocation;
+    private ConsoleColor hBorderColor;
+
+    public ConsoleRectangle(int width, int hieght, Point location, ConsoleColor borderColor)
+    {
+        hWidth = width;
+        hHieght = hieght;
+        hLocation = location;
+        hBorderColor = borderColor;
+    }
+
+    public Point Location
+    {
+        get { return hLocation; }
+        set { hLocation = value; }
+    }
+
+    public int Width
+    {
+        get { return hWidth; }
+        set { hWidth = value; }
+    }
+
+    public int Hieght
+    {
+        get { return hHieght; }
+        set { hHieght = value; }
+    }
+
+    public ConsoleColor BorderColor
+    {
+        get { return hBorderColor; }
+        set { hBorderColor = value; }
+    }
+
+    public void Darw()
+    {
+        string s = "╔";
+        string space = "";
+        string temp = "";
+        for (int i = 0; i < Width; i++)
+        {
+            space += " ";
+            s += "═";
+        }
+
+        for (int j = 0; j < Location.X ; j++)
+            temp += " ";
+
+        s += "╗" + "\n";
+
+        for (int i = 0; i < Hieght; i++)
+            s += temp + "║" + space + "║" + "\n";
+
+        s += temp + "╚";
+        for (int i = 0; i < Width; i++)
+            s += "═";
+
+        s += "╝" + "\n";
+
+        Console.ForegroundColor = BorderColor;
+        Console.CursorTop = hLocation.Y;
+        Console.CursorLeft = hLocation.X;
+        Console.Write(s);
+        Console.ResetColor();
+    }
+}
+https://stackoverflow.com/questions/6006618/how-to-draw-a-rectangle-in-console-application
+*/
